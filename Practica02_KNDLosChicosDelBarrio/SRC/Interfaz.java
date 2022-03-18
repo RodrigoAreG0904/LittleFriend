f7c68414b156a6b9ff099eacf9e85023c7722162
RodrigoAreG0904 <rodrigoareg@ciencias.unam.mx>
2022-03-18 00:32:40 -0600

    Idea para interfaz

--- Practica02_KNDLosChicosDelBarrio/SRC/Interfaz.java
+++ Practica02_KNDLosChicosDelBarrio/SRC/Interfaz.java
@@ -7,32 +7,31 @@ import java.util.Scanner;
  * @author poner a todos
  * @version 16/03/2022
  */
-public class EjemploPractica2 {
+public class Interfaz {
+    //faltan cosas
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-      boolean bandera;
-      int opcion = 0;
-      Scanner input = new Scanner(System.in);
-      ArrayList<Mascota> mascotas = new ArrayList<>();
-      MascotaArchivo archivo = new MascotaArchivo();
-      do {
-        bandera = true;
-        System.out.println("Ingresa la opcion deseda\n 1-CargarDatos\n 2-CrearNuevo\n 3-Salir");
-        try{
-          opcion = input.nextInt();
-        } catch (InputMismatchException e) {
-          input.next();
-          System.out.println("Ingresa un número como opción");
-        }
-        if(opcion > 0 && opcion < 4){
-          switch(opcion){
-            case 1:
-            System.out.println("Cargando datos de Mascotas...");
+    public void hazInterfaz(String opcion){
+        ArrayList<Duenio> duenio = new ArrayList<>();
+        DuenioArchivo archivo = new DuenioArchivo();
+        ArrayList<Estetica> estetica = new ArrayList<>();
+        EsteticaArchivo archivo = new EsteticaArchivo();
+
+        if(opcion.equals("Mascota")){
+            ArrayList<Mascota> mascotas = new ArrayList<>(;
+            MascotaArchivo archivo = new MascotaArchivo();
+            System.out.println("Ingresa la opcion deseda\n 1-CargarDatos\n 2-CrearNuevo\n 3-Salir");
             try{
-              mascotas = archivo.leeOperadores();
+            opcion = input.nextInt();
+            } catch (InputMismatchException e) {
+            input.next();
+            System.out.println("Ingresa un número como opción");
+            }
+            if(opcion > 0 && opcion < 4){
+            switch(opcion){
+                case 1:
+                System.out.println("Cargando datos de Mascotas...");
+                try{
+              mascotas = archivo.leeMascotas();
               System.out.println("Listo...");
             }catch(Exception e){
               System.out.println(e);
@@ -52,25 +51,30 @@ public class EjemploPractica2 {
                   case 1:
                   Mascota mascota = new Mascota();
                   mascotas.add(mascota);
-                  System.out.println("Se agrego el Mascota");
+                  System.out.println("Se agrego la Mascota");
                   break;
 
                   case 2:
-                  for(int i=0;i<mascotas.size();i++){
-                    System.out.println(mascotas.get(i).toString());
+                  for(int i=0;i<operadores.size();i++){
+                    System.out.println(operadores.get(i).toString());
                   }
                   break;
 
                   case 3:
+                  //dejar que edite cada uno de los atributos de la mascota, 
+                  //pero que haya una opción para dejar el atributo que se le deja para editar como esta, hacer un mini menu para que elija que quiere editar, o si quiere editar todo.
                   break;
 
                   case 4:
+                  //metodo para buscar mascota y que todos los datos correspondan a una única mascota
+                  mascotas.remove(mascota);
+                  System.out.println("Se elimino la Mascota");
                   break;
 
                   case 5:
                   if(!mascotas.isEmpty()){
                     System.out.println("Guardando datos de mascotas...");
-                    archivo.escribeOpeador(mascotas);
+                    archivo.escribeMascota(mascotas);
                     System.out.println("Datos guardados");
                   }
                   bandera = false;
@@ -87,7 +91,7 @@ public class EjemploPractica2 {
             do{
               bandera = true;
               System.out.println("Ingresa la opción deseada\n" + "1-Agregar Mascota\n" + "2-Ver Mascotas\n"
-              + "3-Editar Mascota\n" + "4-Borrar Mascota\n" + "5-Salir\n");
+              + "3-Editar Mascotas\n" + "4-Borrar Mascota\n" + "5-Salir\n");
               try {
                 opcion = input.nextInt();
               } catch (InputMismatchException e) {
@@ -99,7 +103,7 @@ public class EjemploPractica2 {
                   case 1:
                   Mascota mascota = new Mascota();
                   mascotas.add(mascota);
-                  System.out.println("Se agrego el Mascota");
+                  System.out.println("Se agrego la Mascota");
                   break;
 
                   case 2:
@@ -115,9 +119,9 @@ public class EjemploPractica2 {
                   break;
 
                   case 5:
-                  if(!mascotas.isEmpty()){
+                  if(!operadores.isEmpty()){
                     System.out.println("Guardando datos de mascotas...");
-                    archivo.escribeOpeador(mascotas);
+                    archivo.escribeMascota(mascotas);
                     System.out.println("Datos guardados");
                   }
                   bandera = false;
@@ -136,8 +140,5 @@ public class EjemploPractica2 {
             break;
           }
         }
-
-      } while (bandera);
-
     }
-  }
+}
