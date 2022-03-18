7485e86f16aea21fbf1e7ff7732ec925e99f4404
dibeher <bere.siempre@gmail.com>
2022-03-17 21:07:34 -0600

    Probando probando

--- Practica02_KNDLosChicosDelBarrio/SRC/LeeryEscribir.java
+++ Practica02_KNDLosChicosDelBarrio/SRC/LeeryEscribir.java
@@ -1,3 +1,7 @@
+/**
+Probando.. probando.. git..
+*/
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
