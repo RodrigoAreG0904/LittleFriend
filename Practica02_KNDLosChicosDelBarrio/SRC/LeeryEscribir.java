c678ce581ae0bfd1282bcb1459618e57c6187134
RodrigoAreG0904 <rodrigoareg@ciencias.unam.mx>
2022-03-17 21:11:02 -0600

    Dejando limpio

--- Practica02_KNDLosChicosDelBarrio/SRC/LeeryEscribir.java
+++ Practica02_KNDLosChicosDelBarrio/SRC/LeeryEscribir.java
@@ -1,11 +1,3 @@
-
-/**
-Probando.. probando.. git..
-*/
-
-//Hola Bere 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
