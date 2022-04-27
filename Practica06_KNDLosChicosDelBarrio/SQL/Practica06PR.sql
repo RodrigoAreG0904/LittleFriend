efcfa1e350f3a0a900645ed7c2f4d6a704ef8f86
RodrigoAreG0904 <rodrigoareg@ciencias.unam.mx>
2022-04-27 14:32:56 -0500

    parte verde completa y cambio de lugar de entidades

--- Practica06_KNDLosChicosDelBarrio/SQL/Practica06PR.sql
+++ Practica06_KNDLosChicosDelBarrio/SQL/Practica06PR.sql
@@ -26,7 +26,8 @@ CREATE TABLE consultaEmergencia(
 	idConsulta INT NOT NULL UNIQUE,
 	idMascota INT NOT NULL UNIQUE,
 	idRecibo INT NOT NULL UNIQUE,
-	procedimientoEmitido motivo VARCHAR(1000) NOT NULL CHECK(procedimientoEmitido <> ''),
+	procedimientoEmitido VARCHAR(1000) NOT NULL CHECK(procedimientoEmitido <> ''),
+	motivo VARCHAR(1000) NOT NULL CHECK(procedimientoEmitido <> ''),
 	sintomas VARCHAR(500) NOT NULL CHECK(sintomas <> ''),
 	codigoVerde BOOLEAN NOT NULL,
 	codigoAmarillo BOOLEAN NOT NULL,
@@ -61,39 +62,6 @@ CREATE TABLE darConsultaNormal(
 	curp CHAR(18) NOT NULL UNIQUE CHECK(CHAR_LENGTH(curp) = 18),
 	idConsulta INT NOT NULL UNIQUE
 );
-
-/*
-Integridad Referencial
-*/
-
---Llaves foraneas
-
-ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_pkey PRIMARY KEY (idConsulta);
-ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_fkey1 FOREIGN KEY (idMascota)
-REFERENCES mascota(idMascota);
-ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_fkey2 FOREIGN KEY (idRecibo)
-REFERENCES recibo(idRecibo);
-
-ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_pkey PRIMARY KEY (idConsulta);
-ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_fkey1 FOREIGN KEY (idMascota)
-REFERENCES mascota(idMascota);
-ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_fkey2 FOREIGN KEY (idRecibo)
-REFERENCES recibo(idRecibo);
-
-ALTER TABLE producto ADD CONSTRAINT producto_pkey PRIMARY KEY (idConsulta);
-ALTER TABLE producto ADD CONSTRAINT producto_fkey FOREIGN KEY (idRecibo)
-REFERENCES recibo(idRecibo);
-
-ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_pkey PRIMARY KEY (idConsulta);
-ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_fkey1 FOREIGN KEY (idMascota)
-REFERENCES mascota(idMascota);
-ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_fkey2 FOREIGN KEY (idRecibo)
-REFERENCES recibo(idRecibo);
-
-ALTER TABLE darConsultaNormal ADD CONSTRAINT darConsultaNormal_fkey1 FOREIGN KEY (curp)
-REFERENCES veterinarios(curp);
-ALTER TABLE darConsultaNormal ADD CONSTRAINT darConsultaNormal_fkey2 FOREIGN KEY (idConsulta)
-REFERENCES consultaNormal(idConsulta);
 
 /*
 Parte Amarilla
@@ -151,6 +119,110 @@ CREATE TABLE mascota(
 );
 
 /*
+Parte Verde
+*/
+CREATE TABLE estilistas(
+	curp CHAR(18) NOT NULL UNIQUE CHECK(CHAR_LENGTH(curp) = 18),
+	idEstetica INT NOT NULL UNIQUE,
+	nombreTrabajador VARCHAR(100) NOT NULL CHECK(nombreTrabajador <> ''),
+	apellidoPaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoPaternoTrabajador <> ''),
+	apellidoMaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoMaternoTrabajador <> ''),
+	estadoTrabajador VARCHAR(100) NOT NULL CHECK(estadoTrabajador <> ''),
+	calleTrabajador VARCHAR(100) NOT NULL CHECK(calleTrabajador <> ''),
+	numeroTrabajador INT NOT NULL,
+	codigoPostalTrabajador INT NOT NULL,
+	salarioTrabajador INT NOT NULL,
+	fechaDeNacimientoTrabajador DATE CHECK(fechaDeNacimientoTrabajador > '2000-10-01'),
+	generoTrabajador VARCHAR(100) NOT NULL CHECK(generoTrabajador <> ''),
+	numeroDeCertificadoTrabajador INT NOT NULL
+);
+
+CREATE TABLE telefonoEstilistas(
+	curp CHAR(18) NOT NULL CHECK(CHAR_LENGTH(curp)=18),
+	telefonoEstilistas CHAR(10) CHECK (telefonoEstilistas SIMILAR TO '[0-9]*')
+);
+
+CREATE TABLE veterinarios(
+	curp CHAR(18) NOT NULL UNIQUE CHECK(CHAR_LENGTH(curp) = 18),
+	idEstetica INT NOT NULL UNIQUE,
+	nombreTrabajador VARCHAR(100) NOT NULL CHECK(nombreTrabajador <> ''),
+	apellidoPaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoPaternoTrabajador <> ''),
+	apellidoMaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoMaternoTrabajador <> ''),
+	estadoTrabajador VARCHAR(100) NOT NULL CHECK(estadoTrabajador <> ''),
+	calleTrabajador VARCHAR(100) NOT NULL CHECK(calleTrabajador <> ''),
+	numeroTrabajador INT NOT NULL,
+	codigoPostalTrabajador INT NOT NULL,
+	salarioTrabajador INT NOT NULL,
+	fechaDeNacimientoTrabajador DATE CHECK(fechaDeNacimientoTrabajador > '2000-10-01'),
+	generoTrabajador VARCHAR(100) NOT NULL CHECK(generoTrabajador <> ''),
+	rfcTrabajador VARCHAR(100) NOT NULL CHECK(rfcTrabajador <> ''),
+	numeroDePacientesActivosTrabajador INT NOT NULL, 
+	horaEntradaTrabajador TIME NOT NULL,
+	horaSalidaTrabajador TIME NOT NULL
+);
+
+CREATE TABLE telefonoVeterinarios(
+	curp CHAR(18) NOT NULL CHECK(CHAR_LENGTH(curp)=18),
+	telefonoVeterinarios CHAR(10) CHECK (telefonoVeterinarios SIMILAR TO '[0-9]*')
+);
+
+CREATE TABLE supervisor(
+	curp CHAR(18) NOT NULL UNIQUE CHECK(CHAR_LENGTH(curp) = 18),
+	idEstetica INT NOT NULL UNIQUE,
+	nombreTrabajador VARCHAR(100) NOT NULL CHECK(nombreTrabajador <> ''),
+	apellidoPaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoPaternoTrabajador <> ''),
+	apellidoMaternoTrabajador VARCHAR(100) NOT NULL CHECK(apellidoMaternoTrabajador <> ''),
+	estadoTrabajador VARCHAR(100) NOT NULL CHECK(estadoTrabajador <> ''),
+	calleTrabajador VARCHAR(100) NOT NULL CHECK(calleTrabajador <> ''),
+	numeroTrabajador INT NOT NULL,
+	codigoPostalTrabajador INT NOT NULL,
+	salarioTrabajador INT NOT NULL,
+	fechaDeNacimientoTrabajador DATE CHECK(fechaDeNacimientoTrabajador > '2000-10-01'),
+	generoTrabajador VARCHAR(100) NOT NULL CHECK(generoTrabajador <> ''),
+	rfcTrabajador VARCHAR(100) NOT NULL CHECK(rfcTrabajador <> ''),
+	horaEntradaTrabajador TIME NOT NULL,
+	horaSalidaTrabajador TIME NOT NULL
+);
+
+CREATE TABLE telefonoSupervisor(
+	curp CHAR(18) NOT NULL CHECK(CHAR_LENGTH(curp)=18),
+	telefonoSupervisor CHAR(10) CHECK (telefonoSupervisor SIMILAR TO '[0-9]*')
+);
+
+/*
+Integridad Referencial
+*/
+
+--Llaves foraneas
+
+ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_pkey PRIMARY KEY (idConsulta);
+ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_fkey1 FOREIGN KEY (idMascota)
+REFERENCES mascota(idMascota);
+ALTER TABLE consultaNormal ADD CONSTRAINT consultaNormal_fkey2 FOREIGN KEY (idRecibo)
+REFERENCES recibo(idRecibo);
+
+ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_pkey PRIMARY KEY (idConsulta);
+ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_fkey1 FOREIGN KEY (idMascota)
+REFERENCES mascota(idMascota);
+ALTER TABLE consultaEmergencia ADD CONSTRAINT consultaEmergencia_fkey2 FOREIGN KEY (idRecibo)
+REFERENCES recibo(idRecibo);
+
+ALTER TABLE producto ADD CONSTRAINT producto_pkey PRIMARY KEY (idConsulta);
+ALTER TABLE producto ADD CONSTRAINT producto_fkey FOREIGN KEY (idRecibo)
+REFERENCES recibo(idRecibo);
+
+ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_pkey PRIMARY KEY (idConsulta);
+ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_fkey1 FOREIGN KEY (idMascota)
+REFERENCES mascota(idMascota);
+ALTER TABLE servicioEstetica ADD CONSTRAINT servicioEstetica_fkey2 FOREIGN KEY (idRecibo)
+REFERENCES recibo(idRecibo);
+
+ALTER TABLE darConsultaNormal ADD CONSTRAINT darConsultaNormal_fkey1 FOREIGN KEY (curp)
+REFERENCES veterinarios(curp);
+ALTER TABLE darConsultaNormal ADD CONSTRAINT darConsultaNormal_fkey2 FOREIGN KEY (idConsulta)
+REFERENCES consultaNormal(idConsulta);
+
+/*
 Integridad Referencial Amarillo
 */
 ALTER TABLE cuidar ADD CONSTRAINT cuidar_fkey1 FOREIGN KEY (idEstetica)
@@ -175,6 +247,35 @@ REFERENCES estetica(idEstetica);
 ALTER TABLE supervisar ADD CONSTRAINT supervisar_fkey2 FOREIGN KEY (curp)
 REFERENCES supervisor(curp);
 
+
 ALTER TABLE mascota ADD CONSTRAINT mascota_pkey PRIMARY KEY (idMascota);
 ALTER TABLE mascota ADD CONSTRAINT mascota_fkey1 FOREIGN KEY (curp)
 REFERENCES telefono(curp);
+
+/*
+Integridad Referencial Verde
+*/
+
+ALTER TABLE estilistas ADD CONSTRAINT estilistas_pkey PRIMARY KEY (curp);
+ALTER TABLE estilistas ADD CONSTRAINT estilistas_fkey FOREIGN KEY (idEstetica)
+REFERENCES estetica(idEstetica);
+
+ALTER TABLE veterinarios ADD CONSTRAINT veterinarios_pkey PRIMARY KEY (curp);
+ALTER TABLE veterinarios ADD CONSTRAINT veterinarios_fkey FOREIGN KEY (idEstetica)
+REFERENCES estetica(idEstetica);
+
+ALTER TABLE supervisor ADD CONSTRAINT supervisor_pkey PRIMARY KEY (curp);
+ALTER TABLE supervisor ADD CONSTRAINT supervisor_fkey FOREIGN KEY (idEstetica)
+REFERENCES estetica(idEstetica);
+
+ALTER TABLE telefonoEstilistas ADD CONSTRAINT telefonoEstilistas_pkey PRIMARY KEY (curp,telefonoEstilistas);
+ALTER TABLE telefonoEstilistas ADD CONSTRAINT telefonoEstilistas_fkey FOREIGN KEY (curp)
+REFERENCES estilistas (curp);
+
+ALTER TABLE telefonoVeterinarios ADD CONSTRAINT telefonoVeterinarios_pkey PRIMARY KEY (curp,telefonoVeterinarios);
+ALTER TABLE telefonoVeterinarios ADD CONSTRAINT telefonoVeterinarios_fkey FOREIGN KEY (curp)
+REFERENCES veterinarios (curp);
+
+ALTER TABLE telefonoSupervisor ADD CONSTRAINT telefonoSupervisor_pkey PRIMARY KEY (curp,telefonoSupervisor);
+ALTER TABLE telefonoSupervisor ADD CONSTRAINT telefonoSupervisor_fkey FOREIGN KEY (curp)
+REFERENCES supervisor (curp);
